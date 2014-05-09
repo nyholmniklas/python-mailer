42c1a645d9f050298638e588c7d715a47cc46d5c
Niklas <nyholmniklas@gmail.com>
2014-05-09 16:55:52 +0300

    Re-aranged widget layout

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from pymailer import PyMailer
 class Gui:
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("500x500")
+        self.root.geometry("600x500")
         self.root.title("python-mailer")
         self._mailer = None
         self._init_widgets()
@@ -15,7 +15,7 @@ class Gui:
         
     def _init_widgets(self):
         #Init frames
-        self._input_frame = Frame(self.root, width = "240");
+        self._input_frame = Frame(self.root, width = "600");
         
         #Init labels
         self._name_label = Label(self._input_frame, text="From Name: ")
@@ -52,12 +52,12 @@ class Gui:
         self._name_label.grid(row=0, column=0, padx=5, pady=5, sticky=W)
         self._email_label.grid(row=1, column=0, padx=5, pady=5, sticky=W)
         self._html_label.grid(row=3, column=0, padx=5, pady=5, sticky=W)
-        self._csv_label.grid(row=3, column=2, padx=5, pady=5, sticky=W)
+        self._csv_label.grid(row=4, column=0, padx=5, pady=5, sticky=W)
         self._host_label.grid(row=0, column=2, padx=5, pady=5, sticky=W)
         self._port_label.grid(row=1, column=2, padx=5, pady=5, sticky=W)
         self._subject_label.grid(row=2, column=0, padx=5, pady=5, sticky=W)
-        self._username_label.grid(row=4, column=0, padx=5, pady=5, sticky=W)
-        self._password_label.grid(row=4, column=2, padx=5, pady=5, sticky=W)
+        self._username_label.grid(row=2, column=2, padx=5, pady=5, sticky=W)
+        self._password_label.grid(row=3, column=2, padx=5, pady=5, sticky=W)
         
         #Layout entry widgets
         self._name_entry.grid(row=0, column=1, padx=5, pady=5, sticky=W)
@@ -65,15 +65,15 @@ class Gui:
         self._host_entry.grid(row=0, column=3, padx=5, pady=5, sticky=W)
         self._port_entry.grid(row=1, column=3, padx=5, pady=5, sticky=W)
         self._subject_entry.grid(row=2, column=1, padx=5, pady=5, sticky=W)
-        self._username_entry.grid(row=4, column=1, padx=5, pady=5, sticky=W)
-        self._password_entry.grid(row=4, column=3, padx=5, pady=5, sticky=W)
+        self._username_entry.grid(row=2, column=3, padx=5, pady=5, sticky=W)
+        self._password_entry.grid(row=3, column=3, padx=5, pady=5, sticky=W)
         
         #Layout checkbox
-        self._auth_checkbox.grid(row=5, column=0, padx=5, pady=5, sticky=W)
+        self._auth_checkbox.grid(row=4, column=3, padx=5, pady=5, sticky=W)
         
         #Layout buttons
         self._html_button.grid(row=3, column=1, padx=5, pady=5, sticky=W)
-        self._csv_button.grid(row=3, column=3, padx=5, pady=5, sticky=W)
+        self._csv_button.grid(row=4, column=1, padx=5, pady=5, sticky=W)
         
     def _update_widgets(self):
         self._clear_widgets()
